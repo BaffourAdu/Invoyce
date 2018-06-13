621b071d48f2c569ba11a6c158f85c5d25d83d6e
bboampong <baffouraduboampong@gmail.com>
2018-06-13 10:48:07 +0000

    Initial Commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import SignIn from '@/components/SignIn'
+import Dashboard from '@/components/Dashboard'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'SignIn',
+      component: SignIn
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
+    }
+
+  ]
+})
